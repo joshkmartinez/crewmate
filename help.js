24134cdbb1a8f4c3421b867a834d8696929f24be
Joshua Martinez <joshkmartinez@gmail.com>
2020-10-10 18:06:55 -0700

    add help cmds

--- help.js
+++ help.js
@@ -1,15 +1,25 @@
 module.exports = {
   'help': {
-    description: 'Shows the list of commands or help on specified command.',
+    description: 'Shows the list of commands or help on a specified command.',
     format: 'help [command-name]'
   },
   'ping': {
-    description: 'Checks connectivity with discord\'s servers.',
+    description: 'Measures connectivity with discord\'s servers.',
     format: 'ping'
   },
   'end': {
     aliases: ['stop'],
-    description: 'Ends the Among Us game in the channel.',
+    description: 'Ends the current Among Us game in the channel.',
     format: 'ends'
+  },
+  'start': {
+    aliases: ['begin'],
+    description: 'Starts an Among Us game in the current channel.',
+    format: 'start [game-code]'
+  },
+  'mute': {
+    aliases: ['unmute'],
+    description: 'Mutes/Unmutes everyone that is in voice channel of the game master.',
+    format: 'mute'
   }
 }
